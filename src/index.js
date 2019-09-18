5a6d2c199ee759e544afcdafe64a6cf9a0c5f6f3
Nazar Tytskyi <Nazar_Tytskyi@epam.com>
2019-09-18 13:33:35 +0300

    fixed structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 //import { Provider } from 'react-redux'
 import configureStore from './store';
 import App from './App';
-import Root from './components/Root'
+import Root from './Root'
 
 ReactDOM.render(
  <Root store={configureStore()}>
