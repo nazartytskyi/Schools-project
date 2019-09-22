1bf5a0a0d3c3f5a7033df7e61e31c656edeb0985
Nazar Tytskyi <nazartitskiy@gmail.com>
2019-09-22 22:41:00 +0300

    master cloned

---
+++ client/src/reducers/schools.js
@@ -0,0 +1,16 @@
+export default (state = {}, action) => {
+  switch (action.type) {
+    case 'GET_SCHOOLS_ACTION':
+      return {
+        ...state,
+        data: action.payload
+      };
+    case 'GET_SCHOOLS_EVENTS_ACTION':
+      return {
+        ...state,
+        events: action.payload
+      };
+    default:
+      return state;
+  }
+};
