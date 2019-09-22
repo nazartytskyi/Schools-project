ad90e546ecae228189d00b88fe0e78345d33b953
Nazar Tytskyi <nazartitskiy@gmail.com>
2019-09-23 02:52:55 +0300

    Sorting added

---
+++ client/src/components/pages/SearchPage/ListSearch/ListSearch.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import PropTypes from 'prop-types';         
+import {makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
+
+
+
+function createData(name, zno, vacant, carbs, protein) {
+  return { name, zno, vacant, carbs, protein };
+}
+
+let rows = [];
+
+// const rows = [
+//   createData('Cupcake', 305, 3.7, 67, 4.3),
+//   createData('Donut', 452, 25.0, 51, 4.9),
+//   createData('Eclair', 262, 16.0, 24, 6.0),
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Gingerbread', 356, 16.0, 49, 3.9),
+//   createData('Honeycomb', 408, 3.2, 87, 6.5),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Jelly Bean', 375, 0.0, 94, 0.0),
+//   createData('KitKat', 518, 26.0, 65, 7.0),
+//   createData('Lollipop', 392, 0.2, 98, 0.0),
+//   createData('Marshmallow', 318, 0, 81, 2.0),
+//   createData('Nougat', 360, 19.0, 9, 37.0),
+//   createData('Oreo', 437, 18.0, 63, 4.0),
+// ];
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+const headCells = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'Назва' },
+  { id: 'zno', numeric: true, disablePadding: false, label: 'ЗНО' },
+  { id: 'vacant', numeric: true, disablePadding: false, label: 'Вільні місця' },
+  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' }
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+  const createSortHandler = property => event => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={order}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3)
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2)
+  },
+  table: {
+    minWidth: 750
+  },
+  tableWrapper: {
+    overflowX: 'auto'
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 10,
+    width: 1
+  }
+}));
+
+export default function ListSearch(props) {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+  const [selected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  function handleRequestSort(event, property) {
+    const isDesc = orderBy === property && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+    setOrderBy(property);
+  }
+
+  function handleClick(event) {
+    //open school page
+  }
+
+  function handleChangePage(event, newPage) {
+    setPage(newPage);
+  }
+
+  function handleChangeRowsPerPage(event) {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  }
+
+  
+  if(props.schools){
+    rows = [];
+    props.schools.map(school => rows.push(createData(school.name, 305, 3.7, 67, 4.3)))
+  }
+
+  const isSelected = name => selected.indexOf(name) !== -1;
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>   
+        <div className={classes.tableWrapper}>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>                       
+              {stableSort(rows, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.name);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => handleClick(event, row.name)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.zno}</TableCell>
+                      <TableCell align="right">{row.vacant}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'previous page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'next page',
+          }}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+}
--- client/src/components/pages/SearchPage/SearchPage.js
+++ client/src/components/pages/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import { connect } from 'react-redux';
 import './SearchPage.scss';
 // import propTypes from 'prop-types';
@@ -6,22 +6,88 @@ import './SearchPage.scss';
 // import { Link } from 'react-router-dom';
 import MapSearch from './MapSearch/MapSearch';
 import Filters from './Filters/Filters';
+import ListSearch from './ListSearch/ListSearch';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { getSchools } from './../../../actions/getSchools';
+import { connect } from 'react-redux';
 
-class SearchPage extends Component {
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={2}>{children}</Box>
+    </Typography>
+  );
+}
+
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
+class SearchPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {value: 0}; 
+  }  
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/getData')
+      .then(data => {
+        return data.json()
+      })
+      .then(schools => {
+        return this.setState({...this.state, schools:schools.data})
+      });
   }
 
-
   render() {
+    const value = this.state.value;  
+    const handleChange = (event, newValue) => {
+      this.setState({...this.state, value:newValue});
+    }
     return (
-      <>
-        {/*<Header/>*/}
+      <>   
         <main>
-         <Filters className="filtrers"/>
-         <MapSearch className="search-results"/>
+          <div className="searchbar">
+
+          </div>
+         
+          <Tabs className="tabs" value={value} onChange={handleChange} aria-label="simple tabs example">
+            <Tab label="List" {...a11yProps(0)} />
+            <Tab label="Map" {...a11yProps(1)} />
+          </Tabs>
+          
+        
+          <div className="search-content-wrapper">
+            <Filters className="filtrers"/>
+
+            <TabPanel value={value} index={0}>
+             <ListSearch schools={this.state.schools} className="search-results"/>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              <MapSearch className="search-results"/>
+            </TabPanel>
+          </div>
         </main>
-        {/*<Footer/>*/}
+        
       </>
       );
   }
