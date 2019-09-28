96237dd640c0b05bee8597f2b4d01c4b46a4c684
Volodymyr Bystrov <bystrovv@ukr.net>
2019-09-28 16:11:40 +0300

    Update Layout.js

--- client/src/components/shared/FooterPlusHeader/Layout.js
+++ client/src/components/shared/FooterPlusHeader/Layout.js
@@ -1,17 +1,43 @@
-import Header from '../header/Header'
-import Footer from '../footer/Footer'
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { getSchools } from '../../../actions/getSchools';
+
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  getSchools: () => dispatch(getSchools())
+});
 
 class Layout extends Component {
-  render () {
+  getSchools = () => {
+    this.props.getSchools();
+  };
+
+  componentDidMount() {
+    this.getSchools();
+  }
+
+  render() {
     return (
       <>
         <Header />
         {this.props.children}
         <Footer />
       </>
-  )
-};
+    );
+  }
+}
+
+Layout.propTypes = {
+  getSchools: propTypes.func,
+  children: propTypes.array
 };
 
-export default Layout;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Layout);
