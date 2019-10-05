d512150386c124de3265957542a614b70d9096b2
Hanachi <yarko-pryima@gmail.com>
2019-10-05 05:13:11 +0300

    Old schoolPage added

--- client/src/components/pages/News/News.js
+++ client/src/components/pages/News/News.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './News.scss'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import NewsCard from './NewsCard';
 
 const mapStateToProps = state => ({
   schools: state.schools
@@ -29,55 +21,25 @@ class News extends Component {
   render() {
     const schools = this.props.schools.data || [];
       
-  return (
-    <div className='news-cards'>
-      {schools.map((school, indexSchool) => {
-        return <div key={school.name}>
-          {school.news.map((item, indexNews) => {
-            let SchoolNewsId = `${indexSchool}${indexNews}`;
-            return <div key={item.name} >
-              <Card className='card' key={item.name}>
-                <CardHeader
-                  title={item.title}
-                  subheader={item.date}
+    return (
+      <div className='news-cards'>
+        {schools.map((school, indexSchool) => {
+          return <div key={school.name}>
+            {school.news.map((item, indexNews) => {
+              let SchoolNewsId = `${indexSchool}${indexNews}`;
+              return <div key={item.name} >
+                <NewsCard 
+                  item={item} 
+                  SchoolNewsId={SchoolNewsId} 
+                  state={this.state} 
+                  handleExpandClick={this.handleExpandClick}
                 />
-                <CardMedia
-                  className='media'
-                  image={item.img}
-                  title={item.title}
-                />
-                <CardContent>
-                  <Typography className='text-ellipsis' variant="body2" color="textSecondary" component="span">
-                      {item.description}
-                  </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                  <IconButton
-                    className={ this.state.expanded.has(SchoolNewsId)
-                      ? 'expandOpen' : 'expand' }
-                    onClick={()=>{this.handleExpandClick(SchoolNewsId)}}
-                    aria-expanded={this.state.expanded}
-                    aria-label="show more"
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </CardActions>
-                <Collapse in={ this.state.expanded.has(SchoolNewsId) ? 
-                  true : 
-                  false} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    <Typography paragraph>
-                      {item.description}
-                    </Typography>
-                  </CardContent>
-                </Collapse> 
-              </Card>
-            </div>
-          })}
-        </div>
-      })}
-  </div>
-  );
+              </div>
+            })}
+          </div>
+        })}
+    </div>
+    );
   }
 }
 
---
+++ client/src/components/pages/SchoolPage/SchoolInfo.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { Container } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import { ButtonGroup } from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import './SchoolInfo.scss';
+
+const mapStateToProps = state => ({
+  schools: state.schools.data
+});
+
+class SchoolInfo extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    const schools = this.props.schools || [];
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg">
+          <div className="school-btn-container"> 
+            <Typography className="school-name" variant="h4" color="textPrimary">
+              {schools[0] ? schools[0].name : 'loading...'}
+            </Typography>
+            <ButtonGroup variant="contained" className='school-btns'>
+              <Button color="primary" className='btn-chosen'>
+                Add to chosen
+              </Button>
+              <Button color="secondary" className='btn-send-doc'>
+                Send documents
+              </Button>
+            </ButtonGroup>
+          </div>
+          {/* <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} /> */}
+          <div className='school-content'>
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <CardMedia
+                  className='media'
+                  image={schools[0] ? schools[0].news[0].img : 'loading...'}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className="paper-info">
+                  <Typography className="info h5" variant="h5">
+                    <b>Info:</b>
+                  </Typography>
+                  <Typography>
+                    <p>Name:  {schools[0] ? schools[0].name : 'loading...'}</p>
+                    <p>Description: {schools[0] ? schools[0].description : 'loading...'}</p>
+                    <p><b>Address:</b></p> 
+                    <p>City:  {schools[0] ? schools[0].adress.city : 'loading...'} </p>  
+                    <p>District:  {schools[0] ? schools[0].adress.district : 'loading...'}</p>
+                    <p>Street:  {schools[0] ? schools[0].adress.street : 'loading...'}</p>
+                    <p>Building :  {schools[0] ? schools[0].adress.building : 'loading...'}</p>     
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper className="paper-info-bottom">
+                  <Typography className="info h5" variant="h5">
+                    Detailed Information
+                  </Typography>
+                  <Typography>
+                    <p>Name:  {schools[0] ? schools[0].news[0].description : 'loading...'}</p>
+                  </Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </React.Fragment> 
+    )
+  }
+}
+
+SchoolInfo.propTypes = {
+  schools: PropTypes.object.isRequired
+}
+export default connect(mapStateToProps)(SchoolInfo);
