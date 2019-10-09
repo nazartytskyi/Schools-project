8952023878bfb1f79df8c15d8a1d2f16df2d6b3c
Nazar Tytskyi <nazartitskiy@gmail.com>
2019-10-10 02:04:22 +0300

    Main page: Links added

--- client/src/components/pages/MainPage/ButtonBases/ButtonBases.js
+++ client/src/components/pages/MainPage/ButtonBases/ButtonBases.js
@@ -12,18 +12,26 @@ export default class ButtonBases extends React.Component {
   render() {
     return (
       <div className="buttons-wrapper">
-        <div className="button">
-          <div className="button__icon"><SearchIcon color="primary"/></div>
-          <h3 className="button__text">Знайти школу</h3>
-        </div>
-        <div className="button">
-          <div className="button__icon"><WorkIcon color="primary"/></div>
-          <h3 className="button__text">Вакансії</h3>
-        </div>
-        <div className="button">
-          <div className="button__icon"><TrendingUpIcon color="primary"/></div>
-          <h3 className="button__text">Рейтинги шкіл</h3>
-        </div>
+        <Link to="/search">
+          <div className="button">
+            <div className="button__icon"><SearchIcon color="primary"/></div>
+            <h3 className="button__text">Знайти школу</h3>
+          </div>
+        </Link>
+
+        <Link to="/vacancies">
+          <div className="button">
+            <div className="button__icon"><WorkIcon color="primary"/></div>
+            <h3 className="button__text">Вакансії</h3>
+          </div>
+        </Link>
+
+        <Link to="/search">  
+          <div className="button">
+            <div className="button__icon"><TrendingUpIcon color="primary"/></div>
+            <h3 className="button__text">Рейтинги шкіл</h3>
+          </div>
+        </Link>
       </div>
     );
   }
