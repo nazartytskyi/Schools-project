7efca5dae785fa25d3295811bba7690042185932
Nazar Tytskyi <nazartitskiy@gmail.com>
2019-10-10 01:48:13 +0300

    main page styled

--- client/src/Root.js
+++ client/src/Root.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import App from './App'
 import MainPage from './components/pages/MainPage/MainPage';
 import Search from './components/pages/SearchPage/SearchPage';
 import Layout from './components/shared/FooterPlusHeader/Layout';
@@ -14,8 +13,7 @@ const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Layout>
-        <Route path='/main' component={MainPage} />
-        <Route path='/app' component={App} />
+        <Route path='/app' component={MainPage} />
         <Route path='/search' component={Search} />
         <Route path='/news' component={News} />
         <Route path='/vacancies' component={Vacancies} />
