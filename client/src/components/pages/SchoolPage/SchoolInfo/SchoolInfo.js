d36b8252ca8a1528277da1b0aec2c33bbec753a3
Dmytro <kuzivdmytro92@gmail.com>
2019-10-12 17:08:33 +0300

    import addNews btn

--- client/src/components/pages/SchoolPage/SchoolInfo/SchoolInfo.js
+++ client/src/components/pages/SchoolPage/SchoolInfo/SchoolInfo.js
@@ -25,6 +25,7 @@ import PersonIcon from '@material-ui/icons/Person';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import './SchoolInfo.scss';
 import '../../../Variables.scss';
+import AddNews from '../../../shared/AddNews/AddNews'
 
 const SchoolInfo = ({currentSchool, addSchool,changeHeart}) => (
       <React.Fragment>
@@ -47,6 +48,7 @@ const SchoolInfo = ({currentSchool, addSchool,changeHeart}) => (
                     Send documents
                   </Button>
                 </ButtonGroup>
+                <AddNews currentSchool={currentSchool}/>
               </div>
               <div className='school-content'>
                 <Grid container spacing={3}>
