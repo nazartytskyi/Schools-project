69d92858285d7a612a86b01018a6edb6fde6f823
Volodymyr Bystrov <bystrovv@ukr.net>
2019-10-18 14:27:55 +0300

    'deploy'

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ db.once('open', () => console.log('connected to the database'));
 
 // checks if connection with the database is successful
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
