d6f7fd71456be443ce6ef7b04688b5eab08e7a8f
Dmytro <kuzivdmytro92@gmail.com>
2019-10-21 02:17:31 +0300

    changed tab title

--- client/src/components/shared/footer/Footer.js
+++ client/src/components/shared/footer/Footer.js
@@ -14,7 +14,6 @@ class Footer extends Component {
             <div className="footer-items">
               <div className="logo">
                 <Link to='/'><img src={logo} alt="logo"/></Link>
-              
               </div>
               <div className="footer-copy">
                 <Typography>&copy; Epam Front-end lab, 2019</Typography>
@@ -22,7 +21,6 @@ class Footer extends Component {
             </div>
           </Container>
         </React.Fragment>
-       
       </footer>  
     )
   }
